7a95b389cde9336fdd28d19e9b16fbaf90dc6cf7
Vishal Sanap <sanapvishal128@gmail.com>
2026-06-23 20:24:08 +0530

    github pages live main.jsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";  // remove this after github pages live link end
+// import { BrowserRouter } from "react-router-dom"; -- enable when not use in github pages
 import "./index.css";
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  // <BrowserRouter>
+  //   <App />
+  // </BrowserRouter>  -- enable when not use in github pages
+
+<HashRouter>
+  <App />
+</HashRouter>
 );
